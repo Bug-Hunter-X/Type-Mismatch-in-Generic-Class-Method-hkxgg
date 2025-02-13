a8ddc76205f4cf010ab773eab7a23ed9100e4876
BugHunter-X <askzenai@gmail.com>
2025-02-13 03:26:04 +0100

    Add Bug.scala file

---
+++ Bug.scala
@@ -0,0 +1,17 @@
+```scala
+class MyClass[T](val value: T) {
+  def myMethod(other: MyClass[T]): Boolean = {
+    value == other.value 
+  }
+}
+
+object Main extends App {
+  val obj1 = new MyClass[Int](5)
+  val obj2 = new MyClass[Int](5)
+  val obj3 = new MyClass[String]("hello")
+  val obj4 = new MyClass[String]("hello")
+  println(obj1.myMethod(obj2)) // true
+  println(obj3.myMethod(obj4)) // true
+  println(obj1.myMethod(obj3)) // Compilation error
+}
+```
